302f214641379dd447f879b57ed6c1b49561b8ce
Etienne Millon <me@emillon.org>
2014-04-06 14:36:38 +0200

    add missing attributes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 HTML5 Gamepad bindings for js_of_ocaml
 ======================================
 
+### Links
+
 [Reference](https://dvcs.w3.org/hg/gamepad/raw-file/default/gamepad.html)
--- gamepad_types.ml
+++ gamepad_types.ml
@@ -1,6 +1,9 @@
 class type gamepad = object
   method id : Js.js_string Js.t Js.readonly_prop
+  (*readonly    attribute long                index;*)
+  (*readonly    attribute boolean             connected;*)
   method timestamp : int Js.readonly_prop
+  (*readonly    attribute DOMString           mapping;*)
   method axes : float Js.js_array Js.t Js.readonly_prop
   method buttons : bool Js.js_array Js.t Js.readonly_prop
 end
