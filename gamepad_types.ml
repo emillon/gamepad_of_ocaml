42e0286fe563e5f81f954b9ae8fe715176e964fc
Etienne Millon <me@emillon.org>
2014-04-06 13:33:23 +0200

    properties are readonly

--- gamepad_types.ml
+++ gamepad_types.ml
@@ -1,5 +1,5 @@
 class type gamepad = object
-  method id : Js.js_string Js.t Js.prop
-  method axes : float Js.js_array Js.t Js.prop
-  method buttons : bool Js.js_array Js.t Js.prop
+  method id : Js.js_string Js.t Js.readonly_prop
+  method axes : float Js.js_array Js.t Js.readonly_prop
+  method buttons : bool Js.js_array Js.t Js.readonly_prop
 end
