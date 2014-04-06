efa370b1e6ab9cd6d42760c9824b3c456066166d
Etienne Millon <me@emillon.org>
2014-04-06 11:49:49 +0200

    bind axes method

--- gamepad_types.ml
+++ gamepad_types.ml
@@ -1,3 +1,4 @@
 class type gamepad = object
   method id : Js.js_string Js.t Js.prop
+  method axes : float Js.js_array Js.t Js.prop
 end
--- jsapp.ml
+++ jsapp.ml
@@ -5,15 +5,22 @@ let status =
   Js.Opt.get (H.document##getElementById(js"status")) (fun () -> assert false)
 
 let display msg =
-  status##innerHTML <- msg
+  status##innerHTML <- js msg
+
+let string_of_gamepad (g:Gamepad_types.gamepad Js.t) =
+  let print_double_array x =
+    "[" ^ String.concat ";" (List.map string_of_float (Array.to_list x)) ^ "]"
+  in
+  Printf.sprintf ("id='%s' axes=%s")
+    (Js.to_string g##id)
+    (g##axes |> Js.to_array |> print_double_array)
 
 let runAnimation () =
   let gamepads = Gamepad.getGamepads () in
   for i = 0 to gamepads##length - 1 do
     let go = Js.array_get gamepads i in
     Js.Optdef.iter go (fun g ->
-      let i = g##id in
-      display i
+      display (string_of_gamepad g)
     )
   done
 
