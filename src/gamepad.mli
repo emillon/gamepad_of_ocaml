28e2ae6e662f40d3917778ade36d60ee290b7217
Etienne Millon <me@emillon.org>
2017-02-01 21:38:28 +0100

    Merge gamepad_types into gamepad

--- src/gamepad.mli
+++ src/gamepad.mli
@@ -1,3 +1,15 @@
-val getGamepads : unit -> Gamepad_types.gamepad Js.t Js.js_array Js.t
+type button
 
-val button_is_pressed : Gamepad_types.gamepadButton -> bool
+class type t = object
+  method id : Js.js_string Js.t Js.readonly_prop
+  (*readonly    attribute long                index;*)
+  (*readonly    attribute boolean             connected;*)
+  method timestamp : int Js.readonly_prop
+  (*readonly    attribute DOMString           mapping;*)
+  method axes : float Js.js_array Js.t Js.readonly_prop
+  method buttons : button Js.js_array Js.t Js.readonly_prop
+end
+
+val get : unit -> t Js.t Js.js_array Js.t
+
+val button_is_pressed : button -> bool
--- src/gamepad_types.mli
+++
@@ -1,1 +0,0 @@
-gamepad_types.ml
